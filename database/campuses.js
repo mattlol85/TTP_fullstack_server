27012c5819ca495dfd2ae75ffa675a827b8157b2
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-23 16:45:42 -0500

    Create Campuses Model

---
+++ database/campuses.js
@@ -0,0 +1,72 @@
+const sequelize = require ("sequelize")
+const db = require('./database')
+
+//Campuses Table (Model Instance)
+const Campuses = db.define('Campuses', {
+    id: {
+        type: sequelize.INTEGER,
+        primaryKey: true
+    },
+    name: {
+        type: sequelize.STRING,
+        allowNull: false,
+        //If the validation fails, no SQL query will be sent to the database at all
+        validate: {
+            notNull: {
+                msg: 'Enter Campus Name'
+            }
+        }
+    },
+    address: {
+        type: sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'Enter The Address'
+            }
+        }
+    },
+    city: {
+        type: sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'Enter The City'
+            }
+        }
+    },
+    state: {
+        type: sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'Enter The State'
+            }
+        }
+    },
+    zip: {
+        type: sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            //Check zip
+            check(zip) {
+                if (!(/^\d{5}$/.test(zip))) {
+                    throw new Error('Invalid Zipcode!!!')
+                }
+            }
+        }
+    },
+    description: {
+        type: sequelize.TEXT
+    }, 
+    img: {
+        type: sequelize.STRING,
+        defaultValue:"url.com"
+    },
+},
+{
+    //This means that direct SQL queries will not cause those strributes to be updated automatically
+    timestamps: false
+})
+
+module.exports = Campuses
